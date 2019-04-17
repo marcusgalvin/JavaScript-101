36094ffeca7d400ad60378e1bb0890ce706e6a82
Marcus Galvin <marcusgalvin.student@careerdevs.com>
2019-04-17 17:34:12 +0000

    Key Value Pairs

---
+++ keyval.js
@@ -0,0 +1,34 @@
+// Directions
+
+// Using the same syntax, we can also add new key-value pairs to objects. We've created a foods object with three entries. 
+// Add three more entries: bananas with a value of 13, grapes with a value of 35, and strawberries with a value of 27.
+
+
+//Given Code:
+
+let foods = {
+  apples: 25,
+  oranges: 32,
+  plums: 28
+};
+
+// change code below this line
+
+// change code above this line
+
+console.log(foods);
+
+
+//Solution
+
+let foods = {
+  apples: 25,
+  oranges: 32,
+  plums: 28
+};
+// change code below this line
+foods['bananas'] = 13;
+foods['grapes'] = 35;
+foods['strawberries'] = 27;
+// change code above this line
+console.log(foods);
