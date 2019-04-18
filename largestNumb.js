0d00445193dadd018f0bfb39fcbb136f19371664
Marcus Galvin <marcusgalvin.student@careerdevs.com>
2019-04-18 21:07:14 +0000

    Largest Number in Array

--- largestNumb.js
+++ largestNumb.js
@@ -1,27 +1,12 @@
-//Directions :
-//Return an array consisting of the largest number from each provided sub-array. 
-//For simplicity, the provided array will contain exactly 4 sub-arrays.
-
-
+//Directions: Return an array consisting of the largest number from each provided sub-array. 
 //Given Code
 
-// //function largestOfFour(arr) {
-//   var results = [];
-//   for (var n = 0; n < arr.length; n++) {
-//     var largestNumber = arr[n][0];
-//     for (var sb = 1; sb < arr[n].length; sb++) {
-//       if (arr[n][sb] > largestNumber) {
-//         largestNumber = arr[n][sb];
-//       }
-//     }
-
-//     results[n] = largestNumber;
-//   }
-
-//   return results;
+// function largestOfFour(arr) {
+//   // You can do this!
+//   return arr;
 // }
 
-
+// largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
 
 // Solution
 function largestOfFour(arr) {
