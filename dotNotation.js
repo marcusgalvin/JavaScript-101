aff60aaa6fb2378a3edec59a56e14c6ce2d2cacd
Marcus Galvin <marcusgalvin.student@careerdevs.com>
2019-05-06 17:41:43 +0000

    dotNotation

---
+++ dotNotation.js
@@ -0,0 +1,21 @@
+//Directions:
+//Use dot notation to log results
+
+//Given Code:
+
+let dog = {
+  name: "Spot",
+  numLegs: 4
+};
+
+
+
+
+//Solution:
+let dog = {
+  name: "Spot",
+  numLegs: 4
+};
+// Add your code below this line
+console.log(dog.numLegs);
+console.log(dog.name)
