39fb2b664fdc24740ef3d8ed42e926e4b95308f3
Marcus Galvin <marcusgalvin.student@careerdevs.com>
2019-05-12 19:09:38 +0000

    instanceOf

---
+++ objectProgramming.js
@@ -0,0 +1,24 @@
+//Create a new instance of the House constructor, calling it myHouse and passing a number of bedrooms.
+//Then, use instanceof to verify that it is an instance of House
+
+//Given Code:
+
+/* jshint expr: true */
+
+function House(numBedrooms) {
+  this.numBedrooms = numBedrooms;
+}
+
+// Add your code below this line
+
+//Solution:
+
+/* jshint expr: true */
+
+function House(numBedrooms) {
+  this.numBedrooms = numBedrooms;
+}
+
+// Add your code below this line
+let myHouse = new House(5);
+myHouse instanceof House;
