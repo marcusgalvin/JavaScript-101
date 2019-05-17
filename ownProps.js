c9c705655c9feb43c1ed1263820b95fa3935bdfc
Marcus Galvin <marcusgalvin.student@careerdevs.com>
2019-05-17 07:02:59 +0000

    ownProps

---
+++ ownProps.js
@@ -0,0 +1,26 @@
+// Directions:
+
+// Add the own properties of canary to the array ownProps.
+
+//Given:
+
+function Bird(name) {
+  this.name = name;
+  this.numLegs = 2;
+}
+
+let canary = new Bird("Tweety");
+let ownProps = [];
+// Add your code below this line
+
+
+// Solution:
+
+let canary = new Bird("Tweety");
+let ownProps = [];
+// Add your code below this line
+for(let property in canary) {
+  if(canary.hasOwnProperty(property)) {
+    ownProps.push(property);
+  }
+}
