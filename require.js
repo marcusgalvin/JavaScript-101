ac9b99f08ac3ee0402735cd604ac54b2bb5f692c
Marcus Galvin <marcusgalvin.student@careerdevs.com>
2019-05-19 19:04:47 +0000

    import and require

---
+++ require.js
@@ -0,0 +1,16 @@
+//Directions:
+
+// Add the appropriate import statement that will allow the current file to use the capitalizeString function. 
+//The file where this function lives is called "string_functions", and it is in the same directory as the current file.
+
+//given code:
+
+"use strict";
+capitalizeString("hello!");
+
+
+//solution:
+
+"use strict";
+import { capitalizeString } from "string-functions";
+capitalizeString("hello!");
