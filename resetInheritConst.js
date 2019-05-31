53d2c43eac0bab12e37dac572506ed6ec5b0cf07
Marcus Galvin <marcusgalvin.student@careerdevs.com>
2019-05-31 19:49:29 +0000

    reset inherited constructor prop

---
+++ resetInheritConst.js
@@ -0,0 +1,35 @@
+//Directions:
+
+// Fix the code so duck.constructor and beagle.constructor return their respective constructors.
+
+//Given code:
+
+function Animal() { }
+function Bird() { }
+function Dog() { }
+
+Bird.prototype = Object.create(Animal.prototype);
+Dog.prototype = Object.create(Animal.prototype);
+
+// Add your code below this line
+
+
+
+let duck = new Bird();
+let beagle = new Dog();
+
+//Solution:
+
+function Animal() { }
+function Bird() { }
+function Dog() { }
+
+Bird.prototype = Object.create(Animal.prototype);
+Dog.prototype = Object.create(Animal.prototype);
+
+// Add your code below this line
+Bird.prototype.constructor = Bird;
+Dog.prototype.constructor = Dog;
+
+let duck = new Bird();
+let beagle = new Dog();
