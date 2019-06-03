56a86c4ba0b81ea5906d59a714899624be2aaabb
Marcus Galvin <marcusgalvin.student@careerdevs.com>
2019-06-03 17:18:06 +0000

    reactDOM practice

---
+++ reactDOM.js
@@ -0,0 +1,14 @@
+//Directions:
+
+//  Use the ReactDOM.render() method to render this component to the page. You can pass defined JSX elements directly in as the first argument and use document.getElementById() to select the DOM node to render them to. There is a div with id='challenge-node' available for you to use. Make sure you don't change the JSX constant.
+
+//Solution:
+const JSX = (
+  <div>
+    <h1>Hello World</h1>
+    <p>Lets render this to the DOM</p>
+  </div>
+);
+// change code below this line
+
+ReactDOM.render(JSX,document.getElementById('challenge-node'));
