ce99d924d3342deeafd09122f70bd62b9ae04eaa
Marcus Galvin <marcusgalvin.student@careerdevs.com>
2019-06-03 17:22:08 +0000

    HTMLvsJSX

---
+++ HTMLvsJSX.js
@@ -0,0 +1,9 @@
+//Directions: declare a class of myDiv to the <div> element in JSX instead of HTML
+
+//Solution:
+
+const JSX = (
+  <div className = "myDiv">         
+    <h1>Add a class to this div</h1>
+  </div>
+);
