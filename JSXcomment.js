14eff73d1c3c4ff7171b8e13bd8497127891da78
Marcus Galvin <marcusgalvin.student@careerdevs.com>
2019-06-03 17:13:57 +0000

    comments in JSX

---
+++ JSXcomment.js
@@ -0,0 +1,10 @@
+//Direction: add a JSX comment in the provided <div>
+//Solution:
+
+const JSX = (
+  <div>
+    <h1>This is a block of JSX</h1>
+    <p>Here's a subtitle</p>
+    {/* hello*/}
+  </div>
+);
