1c0d8155035212a54c531e60d4430655e3399f69
Marcus Galvin <marcusgalvin.student@careerdevs.com>
2019-06-04 11:53:21 +0000

    self closing tags in JSX

---
+++ selfClosingJSX.js
@@ -0,0 +1,27 @@
+//Directions:
+
+// Fix the errors in the code editor so that it is valid JSX and successfully transpiles. Make sure you don't change any of the content - you only need to close tags where they are needed.
+
+//Given Code
+
+const JSX = (
+  <div>
+    {/* remove comment and change code below this line
+    <h2>Welcome to React!</h2> <br >
+    <p>Be sure to close all tags!</p>
+    <hr >
+    remove comment and change code above this line */}
+  </div>
+);
+
+//Solution:
+
+const JSX = (
+  <div>
+    
+    <h2>Welcome to React!</h2> <br />
+    <p>Be sure to close all tags!</p>
+    <hr />
+        
+  </div>
+);
