955e67ab29fa5877daa3791aae3ca07acd121b83
Marcus Galvin <marcusgalvin.student@careerdevs.com>
2019-06-06 21:22:26 +0000

    ConCat

---
+++ concat.js
@@ -0,0 +1,26 @@
+//Directions:
+
+// Use the concat method in the nonMutatingConcat function to concatenate attach to the end of original. The function should return the concatenated array.
+
+//Given Code:
+
+function nonMutatingConcat(original, attach) {
+  // Add your code below this line
+  
+  
+  // Add your code above this line
+}
+var first = [1, 2, 3];
+var second = [4, 5];
+nonMutatingConcat(first, second);
+
+//Solution:
+
+function nonMutatingConcat(original, attach) {
+
+  return original.concat(attach); 
+  
+}
+var first = [1, 2, 3];
+var second = [4, 5];
+nonMutatingConcat(first, second);
