942952a38918f03b016d36481fb4581ed96c92f2
Marcus Galvin <marcusgalvin.student@careerdevs.com>
2019-06-10 20:22:13 +0000

    access a nested array

---
+++ accessNestedArrays.js
@@ -0,0 +1,54 @@
+//Directions:
+
+// Retrieve the second tree from the variable myPlants using object dot and array bracket notation.
+
+//Given Code:
+
+var myPlants = [
+  { 
+    type: "flowers",
+    list: [
+      "rose",
+      "tulip",
+      "dandelion"
+    ]
+  },
+  {
+    type: "trees",
+    list: [
+      "fir",
+      "pine",
+      "birch"
+    ]
+  }  
+];
+
+// Only change code below this line
+
+var secondTree = ""; // Change this line
+
+
+//Solution:
+
+var myPlants = [
+  { 
+    type: "flowers",
+    list: [
+      "rose",
+      "tulip",
+      "dandelion"
+    ]
+  },
+  {
+    type: "trees",
+    list: [
+      "fir",
+      "pine",
+      "birch"
+    ]
+  }  
+];
+
+// Only change code below this line
+
+var secondTree = myPlants[1].list[1];
